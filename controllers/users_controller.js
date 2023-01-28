8655f426ed788d9f4325021512d052fede943b2c
Harsha <harsha@123.com>
2023-01-28 10:32:56 +0530

    handkes file uploads with multer

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,7 @@
 
 const User = require('../models/user');
+const fs = require('fs');
+const path = require('path');
 
 module.exports.profile = async function(req,res){
     try{
@@ -17,8 +19,27 @@ module.exports.profile = async function(req,res){
 module.exports.update = async function(req,res){
     try{
         if(req.user.id == req.params.id){
-            await User.findByIdAndUpdate(req.params.id,req.body);
-            return res.redirect('back');
+            let user = await User.findByIdAndUpdate(req.params.id,req.body);
+            User.uploadedAvatar(req,res,function(err){
+
+                if(err){
+                    console.log('****Multer Error****',err);
+                }
+                user.name = req.body.name;
+                user.email = req.body.email;
+                // console.log(req.file);
+                
+                if(req.file){
+
+                    if(user.avatar){
+                        fs.unlinkSync(path.join(__dirname,'..',user.avatar));
+                    }
+
+                    user.avatar = User.avatarPath + '\\' + req.file.filename;
+                }
+                user.save();
+                return res.redirect('back');
+            })
         }
         else{
             return res.status(401).send('Unauthorized');
